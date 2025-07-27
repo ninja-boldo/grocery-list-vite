ae7de26f1020d7d5eb6d8ef32d69b54d882d3ff2
admin <YOUR_EMAIL>
2025-07-27 22:29:40 +0200

    now working backend implementation but i have to implement the global fetching ability and the routing back button in the scanner on the frontend

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Query, Request
 import uvicorn
 
+
 DB_PATH = "data/openfoodfacts.db"
 
 @asynccontextmanager
@@ -21,19 +22,29 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def read_root(request: Request):
-    result = request.app.state.con.execute("select * from openfoodfacts limit 1").fetchall()
-    print(result)
-    return {"message": result}
+    
+    #use this to get info about the names of the columns: DESCRIBE main.food;
+    #code is the ean
+    ean = "5411188128601"
+    columns = request.app.state.con.execute(f"""
+        SELECT code, product_name FROM main.food where code = '{ean}'
+    """).df() 
+    print(columns.head())
+
+    return {"message": columns}
+
+
 
 @app.get("/get_info/")
 async def get_info(request: Request, ean: str = Query(..., min_length=8, max_length=14)):
     con = request.app.state.con
-    cur = con.execute("SELECT * FROM openfoodfacts WHERE ean = ?", (ean,))
+    cur = con.execute("SELECT * FROM food WHERE ean = ?", (ean,))
     row = cur.fetchone()
     return {"ean": ean, "row": row}
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":       
+        
     uvicorn.run(
         "server:app",
         host="127.0.0.1",
