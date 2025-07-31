8e54cd02adc7c8312b6e1680eda5215fe9b379fe
admin <YOUR_EMAIL>
2025-07-31 17:36:19 +0200

    currently more or less working but some strange parent list issues etc.

--- server/test.py
+++ server/test.py
@@ -4,16 +4,21 @@ DB_PATH = "data/openfoodfacts.db"
 
 con = duckdb.connect(DB_PATH)
 
-# Create schema if needed
-con.execute("CREATE SCHEMA IF NOT EXISTS main")
-
-# Create table and import CSV data (replace with actual columns/types or infer automatically)
-con.execute("""
-CREATE TABLE IF NOT EXISTS item_list (
-    item_name string,
-    subgroups string,
-)
-""")
+# # Create schema if needed
+# con.execute("CREATE SCHEMA IF NOT EXISTS main")
 
 
+# con.execute("DROP TABLE IF EXISTS main.item_list")
+
+
+# con.execute("""
+# CREATE TABLE IF NOT EXISTS item_list (
+#     ean string,
+#     item_name string,
+#     subgroups string,
+# )
+# """)
+
+
+print(con.execute("select * from main.item_list").df())
 con.close()
